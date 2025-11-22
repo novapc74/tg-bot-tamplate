58fba3cea81b4fca35eda90596c39a7ccedc8d36
novapc74 <novapc74@yandex.ru>
2025-11-22 21:22:09 +0300

    certbot

--- project/templates/base.php
+++ project/templates/base.php
@@ -1,26 +1,18 @@
 <!DOCTYPE html>
 <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{{ meta_title }}</title>
-
-        {% app_css %}
-
-    </head>
-    <body>
-
-        {% include 'embed/navbar.php' %}
-
-        <main>
-            {% block body %}
-            Default body
-            {% end block %}
-        </main>
-
-        {% include 'embed/footer.php' %}
-
-        {% app_js %}
-
-    </body>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{ meta_title }}</title>
+{% app_css %}
+</head>
+<body>
+    {% include 'embed/navbar.php' %}
+    <main>
+    {% block body %}
+    {% end block %}
+    </main>
+    {% include 'embed/footer.php' %}
+    {% app_js %}
+</body>
 </html>
