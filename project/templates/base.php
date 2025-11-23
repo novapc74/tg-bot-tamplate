6383043784704bf3b45391fa797d1c5d0ee99f36
novapc74 <novapc74@yandex.ru>
2025-11-23 10:02:22 +0300

    add meta description

--- project/templates/base.php
+++ project/templates/base.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>{{ meta_title }}</title>
-{% app_css %}
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Телеграм-бот">
+    <title>{{ meta_title }}</title>
+    {% app_css %}
 </head>
 <body>
     {% include 'embed/navbar.php' %}
