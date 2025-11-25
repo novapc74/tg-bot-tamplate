4335e70b1860f81277e987749fdf1af613efd82b
novapc74 <novapc74@yandex.ru>
2025-11-25 13:10:44 +0300

    add footer

--- project/src/Handlers/CommandHandlers/AsicPriceGeneratorHandler.php
+++ project/src/Handlers/CommandHandlers/AsicPriceGeneratorHandler.php
@@ -77,25 +77,7 @@ final readonly class AsicPriceGeneratorHandler extends AbstractTelegramBotHandle
 
         $prices = str_replace($search, $replace, $prices);
 
-
-        $prices .= <<<EOT
-Другие модели по запросу:
-Окупаемость [ЗДЕСЬ](https://whattomine.com/asics)
-[¦КУПИТЬ¦](https://t.me/jonnyfase)
-[¦Чат¦](https://t.me/Mining_KRD_23)
-[¦Авито¦](https://www.avito.ru/brands/i71930904?src=sharing)
-[¦Отзывы¦](https://t.me/mining_krd_otziv)
-[¦USDT¦](https://rapira.net/?ref=06FL)
-[¦Bybit¦](https://www.bybit.com/invite?ref=RB2PKB)
-[¦Барахолка¦](https://t.me/mining_baraholka23)
-[¦АВТОМОБИЛИ¦](https://t.me/ChinaMotors123)
-[¦ВИДЕОКАРТЫ¦](https://t.me/alimca_cn)
-Отправка по РФ\.
-[Голосовать](https://t.me/boost/mining_KRD123) ✅ за канал
-EOT;
-
-
-        return $prices;
+        return $prices . self::getFooter();
     }
 
     private static function updateItemPrice(int $price): int
@@ -113,5 +95,24 @@ EOT;
         $price -= 1;
 
         return $price;
+    }
+
+    private static function getFooter():string
+    {
+        return <<<EOT
+Другие модели по запросу:
+Окупаемость [ЗДЕСЬ](https://whattomine.com/asics)
+[¦КУПИТЬ¦](https://t.me/jonnyfase)
+[¦Чат¦](https://t.me/Mining_KRD_23)
+[¦Авито¦](https://www.avito.ru/brands/i71930904?src=sharing)
+[¦Отзывы¦](https://t.me/mining_krd_otziv)
+[¦USDT¦](https://rapira.net/?ref=06FL)
+[¦Bybit¦](https://www.bybit.com/invite?ref=RB2PKB)
+[¦Барахолка¦](https://t.me/mining_baraholka23)
+[¦АВТОМОБИЛИ¦](https://t.me/ChinaMotors123)
+[¦ВИДЕОКАРТЫ¦](https://t.me/alimca_cn)
+Отправка по РФ\.
+[Голосовать](https://t.me/boost/mining_KRD123) ✅ за канал
+EOT;
     }
 }
